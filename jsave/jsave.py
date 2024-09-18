f8524d9d55544ac4662548ca61fab5cae4af612b
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2024-09-18 18:40:58 -0500

    Add docstrings to all methods

--- jsave/jsave.py
+++ jsave/jsave.py
@@ -2,12 +2,30 @@ import json
 import os
 
 def save(data: dict, filepath: str):
+    """
+    Saves a python dict to a filepath as JSON data
+
+    Args:
+        data (dict)
+        filepath (string)
+    """
     jsonData = json.dumps(data, indent=4)
     with open(filepath, "w") as f:
         f.write(jsonData)
     
 
 def read(filepath: str, keys: [str] = [], safe_mode: bool = True) -> dict:
+    """
+    Reads a JSON file an return it as a python dict.
+
+    Args:
+        filepath (string)
+        keys ([str]) = []
+        safe_mode (bool) = True
+
+    Returns:
+        Dict with JSON data
+    """
     with open(filepath, "r") as f:
         if keys:
             loaded_dict = json.loads(f.read())
@@ -26,10 +44,25 @@ def read(filepath: str, keys: [str] = [], safe_mode: bool = True) -> dict:
         return json.loads(f.read())
 
 def delete(filepath: str):
+    """
+    Delete a file at the specified filepath.
+
+    Args:
+        filepath (string)
+
+    """
     if os.path.exists(filepath):
         os.remove(filepath)
 
 def merge(files: [str], output_filepath: str):
+    """
+    Merge 2 or more JSON files and combine them into one.
+
+    Args:
+        files ([str])
+        output_filepath (str)
+
+    """
     merged_data = {}
 
     for file in files:
