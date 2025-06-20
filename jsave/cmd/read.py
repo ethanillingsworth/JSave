ee431c96cd22f5e962937de0cbc1c3c6f0e9b45a
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2025-06-20 13:50:57 -0500

    update descriptions

--- jsave/cmd/read.py
+++ jsave/cmd/read.py
@@ -1,7 +1,7 @@
 import argparse
 from ..jsave import JFile, JData
 
-parser = argparse.ArgumentParser(description="save data to a file")
+parser = argparse.ArgumentParser(description="read data from a file")
 
 parser.add_argument("file", type=str, help="filename read the data from")
 parser.add_argument("--indent", type=int, default=0, help="indent to format the data with, use -1 for no formatting")
--- jsave/cmd/update.py
+++ jsave/cmd/update.py
@@ -1,7 +1,7 @@
 import argparse
 from ..jsave import JFile, JData
 
-parser = argparse.ArgumentParser(description="save data to a file")
+parser = argparse.ArgumentParser(description="update data in a file")
 
 parser.add_argument("file", type=str, help="file to update")
 parser.add_argument("key", type=str or int, help="key path to update")
